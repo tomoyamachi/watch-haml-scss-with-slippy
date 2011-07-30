423c65c9f27b9cd37fc58ddf21fa0fbcdad614e2
Tomoya Amachi <simpleplay1217@gmail.com>
2011-07-31 03:48:50 +0900

    change structure

--- watchr.rb
+++ watchr.rb
@@ -1,3 +1,3 @@
-watch('src/(.*)\.haml')   {|md| system("haml -f html5 #{md[0]} src/#{md[1]}.html") }
-watch('src/(.*)\.scss')   {|md| system("sass --scss #{md[0]} src/#{md[1]}.css")    }
+watch('src/(.*)\.haml')   {|md| system("haml -f html5 #{md[0]} src/#{md[1]}/#{md[1]}.html") }
+watch('src/(.*)\.scss')   {|md| system("sass --scss #{md[0]} src/#{md[1]}/#{md[1]}.css")    }
 watch('src/(.*)\.coffee') {|md| system("coffee -o prod -c #{md[0]}")                }
